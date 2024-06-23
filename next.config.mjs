a3ab0987c14c2a0e8f89db6b8c60904420a7c256
Dylan Dorusse-Répi <agence.drdigital@gmail.com>
2024-06-23 16:11:37 +0200

    correction header

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,68 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const cspHeader = `
+    upgrade-insecure-requests
+`
+
+const securityHeaders = [
+    {
+        key: "X-DNS-Prefetch-Control",
+        value: "on",
+    },
+    {
+        key: "Strict-Transport-Security",
+        value: "max-age=63072000; includeSubDomains; preload",
+    },
+    {
+        key: "X-XSS-Protection",
+        value: "1; mode=block",
+    },
+    {
+        key: "X-Frame-Options",
+        value: "sameorigin",
+    },
+    {
+        key: "Permissions-Policy",
+        value: "accelerometer=(), camera=(), cross-origin-isolated=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), usb=()",
+    },
+    {
+        key: "Referrer-Policy",
+        value: "no-referrer-when-downgrade",
+    },
+    {
+        key: "X-Content-Type-Options",
+        value: "nosniff",
+    },
+];
+
+const nextConfig = {
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://api.agorinfo.fr/api/:path*', // Proxy to Backend
+            },
+        ];
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: cspHeader.replace(/\n/g, ''),
+                    },
+                ],
+            },
+            {
+                source: '/(.*)',
+                headers: securityHeaders,
+            },
+        ]
+    },
+
+};
 
 export default nextConfig;
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -31,12 +31,14 @@ const Header = () => {
 
     return (
         <header
-            className="sticky top-0 bg-white z-[999] border-b border-greyscale-lightest shadow-nav overflow-x-hidden lg:overflow-x-visible">
+            className={`sticky top-0 bg-white z-[999] border-b border-greyscale-lightest shadow-nav`}>
             <div
                 className="flex items-center justify-between py-6 relative max-w-screen-xl mx-auto px-4 md:px-8 xl:px-0">
-                <Link className="w-[200px]" href="/"><img className="w-full h-full object-contain"
-                                                          src={logo ? backUrl + logo : emptyImg.src}
-                                                          alt="WeNégoce"/></Link>
+                <Link className="w-[200px]" href="/">
+                    <img className="w-full h-full object-contain"
+                         src={logo ? backUrl + logo : emptyImg.src}
+                         alt="WeNégoce"/>
+                </Link>
                 <Nav navItems={data.navItems} isOpen={isOpen} setIsOpen={setIsOpen}/>
                 <ModalButton label={"Nous contacter"} className="btn btn-accent hidden lg:inline-flex">
                     <ContactForm/>
