1cca68340cc567ea20bbfe5bc4a823d2bb89fa1e
Dylan Dorusse-Repi <agence.drdigital@gmail.com>
2025-05-30 14:14:00 +0200

    correction hero

--- src/app/solutions/page.tsx
+++ src/app/solutions/page.tsx
@@ -12,24 +12,24 @@ import type {Metadata} from "next";
 import getGlobal from "@/actions/getGlobal";
 
 export const generateMetadata = async (): Promise<Metadata> => {
-    const {BACK_URL} = process.env;
+    const {BACK_URL, FRONT_URL} = process.env;
     const global = await getGlobal();
     const metas = global.archiveSolutions.metas
 
     return {
-        metadataBase: new URL(metas.canonicalUrl),
-        title: metas.meta_title || "Agorinfo, éditeur de solution logicielles métier",
+        metadataBase: new URL(FRONT_URL + "/solutions" ),
+        title: metas?.meta_title || "Agorinfo, éditeur de solution logicielles métier",
         description: metas?.meta_description || "Solutions logicielles de gestion : logiviande, SILOS , LSA et Comptinnov. Découvrez nos services, conseils, formations pour votre solution logiciele de gestion.",
         openGraph: {
             title: metas?.meta_title || "Agorinfo, éditeur de solution logicielles métier",
             siteName: metas?.meta_title || "Agorinfo, éditeur de solution logicielles métier",
             description: metas?.meta_description || "Solutions logicielles de gestion : logiviande, SILOS , LSA et Comptinnov. Découvrez nos services, conseils, formations pour votre solution logiciele de gestion.",
-            url: metas.canonicalUrl,
+            url: FRONT_URL + "/solutions",
             images: [`${BACK_URL}${metas?.shareImage?.data?.attributes.url}` || ""],
         },
         twitter: {
             card: 'summary_large_image',
-            site: metas.canonicalUrl,
+            site: FRONT_URL + "/solutions",
             title: metas?.meta_title || "Agorinfo, éditeur de solution logicielles métier",
             description: metas?.meta_description || "Solutions logicielles de gestion : logiviande, SILOS , LSA et Comptinnov. Découvrez nos services, conseils, formations pour votre solution logiciele de gestion.",
             images: [`${BACK_URL}${metas?.shareImage?.data?.attributes.url}` || ""],
