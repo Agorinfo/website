859ce95eb9ed4574997abb485efba038bc65ccf4
Dylan Dorusse-Repi <agence.drdigital@gmail.com>
2025-06-02 10:33:00 +0200

    correction contactForm

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -4,13 +4,13 @@ import {useQuery} from "@tanstack/react-query";
 import getGlobal from "@/actions/getGlobal";
 import Loader, {LoaderButton} from "@/components/Loader";
 import Button from "@/components/Button";
-import emptyImg from "@/assets/empty-img.png"
 import toast from "react-hot-toast";
+import {sendMail} from "@/utils/sendEmail";
+import send from "@/actions/SendEmail";
 import useModalStore from "@/store/ModalStore";
-import getFooter from "@/actions/getFooter";
 
 const ContactForm = () => {
-    const backUrl = process.env.NEXT_PUBLIC_BACK_URL;
+    const url = process.env.NEXT_PUBLIC_FRONT_URL;
     const [active, setActive] = useState("formulaire");
     const { closeModal } = useModalStore();
     const [loading, setLoading] = useState(false);
@@ -51,25 +51,19 @@ const ContactForm = () => {
         queryFn: () => getGlobal()
     })
 
-    const footerData = useQuery({
-        queryKey: ["footer"],
-        queryFn: () => getFooter(),
-    });
-
-    const {siteName, street, adressComp, zipCode, city, tel, email, addressComp, portalUrl, logo} = data;
+    const {siteName, street, adressComp, zipCode, city, tel, email} = data;
 
     const telUrl= tel.replaceAll(" ", "").substring(1);
-    const telCompUrl = addressComp?.tel.replaceAll(" ", "").substring(1);
 
-    if(isLoading || footerData.isLoading) return <Loader />
+    if(isLoading) return <Loader />
 
-    if(error || footerData.error) return <p>{error?.message || footerData.error?.message}</p>
+    if(error) return <p>{error.message}</p>
 
     return (
         <div className="flex flex-col lg:flex-row bg-white">
-            <h2 className="text-h3 font-bold capitalize pt-8 px-8 pb-6 lg:hidden">Contacter <span className="text-[#3965bd] font-bold">We</span><span className='font-light text-[#646464]'>Négoce</span></h2>
+            <h2 className="text-h3 font-bold capitalize pt-8 px-8 pb-6 lg:hidden">Contacter {siteName}</h2>
             <div className="lg:hidden px-8 flex">
-                <div className="flex justify-start items-start gap-1 rounded-lg border overflow-hidden">
+                <div className="flex justify-start items-start gap-1 rounded-lg border overflow-clip">
                     <button
                         onClick={() => setActive('formulaire')}
                         className={`px-4 py-3 w-[9.25rem] ${active === "formulaire" ? "text-accent bg-accent-shine" : "text-grayscale-darker"}`}
@@ -87,53 +81,28 @@ const ContactForm = () => {
             <div
                 className={`p-8 flex flex-col justify-between items-start lg:border-r lg:border-grayscale-lighter lg:w-[33.333vw] max-w-[32rem] lg:block ${active === "coordonnees" ? "block" : "hidden lg:block"}`}>
                 <div className="">
-                    <div className="pb-6 hidden lg:block w-[17.5rem]">
-                        <img className="w-full" src={logo.data ? backUrl + logo.data.attributes.url : emptyImg.src}
-                             alt={logo.data.attributes.alternativeText}/>
+                    <div className="pb-6 hidden lg:block">
+                        <img src="/logotype.webp" alt="Agorinfo"/>
                     </div>
-                    <div className="divide-y">
-                        <div className="flex flex-col gap-2 pb-6">
-                            <h3 className="text-h6 font-bold">À Rouen</h3>
-                            <div className="flex flex-col items-start">
-                                <span>{street}</span>
-                                <span>{adressComp}</span>
-                                <span>{zipCode} {city}</span>
-                            </div>
-                            <a className="link-normal" href={"tel:+33" + telUrl}>{tel}</a>
-                            <a className="link-normal" href={"mailto:" + email}>{email}</a>
+                    <div className="flex flex-col gap-2 pb-6">
+                        <h3 className="text-h4 font-bold">{siteName}</h3>
+                        <div className="flex flex-col items-start">
+                            <span>{street}</span>
+                            <span>{adressComp}</span>
+                            <span>{zipCode} {city}</span>
                         </div>
-                        <div className="flex flex-col gap-2 py-6">
-                            <h3 className="text-h6 font-bold">{addressComp?.name}</h3>
-                            <div className="flex flex-col items-start">
-                                <span>{addressComp?.address}</span>
-                                <span>{addressComp?.zipCode} {addressComp?.city}</span>
-                            </div>
-                            <a className="link-normal" href={"tel:+33" + telCompUrl}>{addressComp?.tel}</a>
-                            <a className="link-normal" href={"mailto:" + addressComp?.email}>{addressComp?.email}</a>
-                        </div>
+                        <a className="link-normal" href={"tel:+33" + telUrl}>{tel}</a>
+                        <a className="link-normal" href={"mailto:" + email}>{email}</a>
                     </div>
                 </div>
                 <div className="pb-8">
                     <h4 className="text-h4 font-bold">Besoin d&apos;assistance ? </h4>
                     <p className="text-grayscale-darkest pb-6">Envoyez votre demande au support technique</p>
-                    {portalUrl && <Button label="Créer un ticket" url={portalUrl} className="btn btn-gray"/>}
-                </div>
-                <div className="flex items-center gap-2">
-                    {footerData.data.socials.map((item: any) => (
-                        <Button
-                            ariaLabel={"lien vers " + item.social}
-                            key={item.id}
-                            icon={item.social}
-                            url={item.url}
-                            className="text-2xl hover:text-featured-peps transition-all duration-300 ease-out"
-                        />
-                    ))}
+                    <Button label="Créer un ticket" url={"https://agorinfo.atlassian.net/servicedesk/customer/portals"} className="btn btn-gray"/>
                 </div>
             </div>
             <div className={`p-8 lg:w-[50vw] max-w-[48rem] ${active === "formulaire" ? "block" : "hidden lg:block"}`}>
-                <h2 className="text-h3 font-bold capitalize pb-6 hidden lg:block">Contacter <span
-                    className="text-[#3965bd] font-bold">We</span><span
-                    className='font-light text-[#646464]'>Négoce</span></h2>
+                <h2 className="text-h3 font-bold capitalize pb-6 hidden lg:block">Contacter {siteName}</h2>
                 <form onSubmit={handleSubmit} className="grid sm:grid-cols-2 gap-4">
                     <label className="label-style" htmlFor="firstname">
                         Prénom *
@@ -191,7 +160,7 @@ const ContactForm = () => {
                                 <input
                                     type="radio"
                                     id="contact"
-                                    name="object"
+                                    name="civility"
                                     value="Être recontacté"
                                     className="radio-style"
                                     defaultChecked
@@ -202,7 +171,7 @@ const ContactForm = () => {
                                 <input
                                     type="radio"
                                     id="demo"
-                                    name="object"
+                                    name="civility"
                                     value="Réserver une démo"
                                     className="radio-style"
                                 />
@@ -212,7 +181,7 @@ const ContactForm = () => {
                                 <input
                                     type="radio"
                                     id="other"
-                                    name="object"
+                                    name="civility"
                                     value="Autre demande"
                                     className="radio-style"
                                 />
@@ -231,11 +200,11 @@ const ContactForm = () => {
                             </textarea>
                     </label>
                     <div className="flex items-center justify-between col-span-full">
-                        <button className="btn btn-accent mb-4" type={"submit"} >{loading ? <LoaderButton /> : "Envoyer"}</button>
+                        <button className="btn btn-accent mb-4" type={"submit"}> {loading ? <LoaderButton /> : "Envoyer"}</button>
                         <span className={"text-sm text-grayscale-darker"}>* Obligatoire</span>
                     </div>
                     <p className="text-sm text-grayscale-darker col-span-full">En envoyant ce message, vous acceptez
-                        la <a href="/politique-de-confidentialité">Politique de confidentialité</a>.</p>
+                        la <a href="/politique-de-confidentialite">Politique de confidentialité</a>.</p>
                 </form>
             </div>
         </div>
