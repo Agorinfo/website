ada62ceffcc4826afaa41ea1501c2c752d55d9b1
Dylan Dorusse-Repi <agence.drdigital@gmail.com>
2025-06-03 11:33:53 +0200

    correction ServiceList

--- src/components/RelatedCard.tsx
+++ src/components/RelatedCard.tsx
@@ -1,51 +1,38 @@
-"use client";
+"use client"
 import React from 'react';
-import RelatedRessourceCard from "@/components/RelatedRessourceCard";
-import getRelatedRessources from "@/actions/getRelatedRessouces";
-import Loader from "@/components/Loader";
-import {useQuery} from "@tanstack/react-query";
-import Link from "next/link";
+import Button from "@/components/Button";
+import Icon from "@/components/icons/Icon";
+import {RelatedCardType} from "@/utils/types";
+import {Check} from "@phosphor-icons/react";
+import clsx from "clsx";
 
-const RelatedRessources = ({ressourceId}: {ressourceId: number}) => {
-    const {data: ressources, error, isLoading} = useQuery({
-        queryKey: ["related-ressources", ressourceId],
-        queryFn: async () => await getRelatedRessources( ressourceId),
-    });
-
-    if (isLoading) return <Loader />
-
-    if (error) return <p>{error?.message}</p>
+const RelatedCard = ({icon, title, text, listItems, label, url, className}: RelatedCardType) => {
 
     return (
-        <>
-            {ressources?.length > 0 &&
-                <div>
-                    <h3 className="text-h3 pb-12 font-bold text-center">Autres ressources</h3>
-                    <div className="flex flex-col gap-12 pb-8 lg:pb-12">
-                        {ressources?.map((ressource: any) => (
-                            <RelatedRessourceCard
-                                key={ressource.id}
-                                slug={ressource.attributes.slug}
-                                color={ressource.attributes.category?.data?.attributes.color}
-                                thumbnail={ressource.attributes.featuredImage?.data?.attributes?.formats?.small
-                                    ? ressource.attributes.featuredImage.data?.attributes.formats.small.url
-                                    : ressource.attributes.featuredImage.data?.attributes.url}
-                                alt={ressource.attributes.featuredImage?.data?.attributes?.alternativeText}
-                                category={ressource.attributes.category?.data?.attributes.name}
-                                title={ressource.attributes.title}
-                                date={ressource.attributes.publishedAt}
-                            />
-                        ))}
-                    </div>
-                    <div className="text-center">
-                        <Link href={`/ressources`} className="btn btn-accent">
-                            Voir plus
-                        </Link>
-                    </div>
-                </div>
-            }
-        </>
+        <div className={clsx("relative px-4 pb-4 pt-6 border border-grayscale-lighter rounded-lg flex flex-col gap-4", className)}>
+            <span
+                className="size-12 rounded-xl bg-accent-muted text-accent inline-flex justify-center items-center absolute top-[-0.625rem] left-4">
+                <Icon
+                    size={24}
+                    name={icon}
+                />
+            </span>
+            <h3 className="text-center text-featured text-h4 font-bold">{title}</h3>
+            <p className="paragraph max-w-[18rem] mx-auto text-center pb-8">{text}</p>
+            {listItems &&
+                <ul>
+                    {listItems.map(item => (
+                        <li key={item.id} className="flex items-center gap-2 text-grayscale-darkest "><Check
+                            className="text-accent" size={24}/>{item.listItem}</li>
+                    ))}
+                </ul>}
+            <Button
+                label={label}
+                url={url}
+                className="btn btn-accent mt-auto"
+            />
+        </div>
     );
 };
 
-export default RelatedRessources;
+export default RelatedCard;
