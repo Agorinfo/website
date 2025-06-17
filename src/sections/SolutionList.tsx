d43f0e409c014a8f29c1d21e1e16d30667827263
Dylan Dorusse-Repi <agence.drdigital@gmail.com>
2025-06-17 15:01:04 +0200

    correction divers

--- src/sections/SolutionList.tsx
+++ src/sections/SolutionList.tsx
@@ -1,11 +1,13 @@
 "use client"
-import React from 'react';
+import React, {useState} from 'react';
 import {useQuery} from "@tanstack/react-query";
 import RelatedCard from "@/components/RelatedCard";
 import Loader from "@/components/Loader";
 import {useKeenSlider} from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 import getSolutions from "@/actions/getSolutions";
+import clsx from "clsx";
+import {ArrowLeft, ArrowRight} from "@phosphor-icons/react";
 
 type RelatedCardType = {
     id: number;
@@ -23,12 +25,21 @@ type RelatedCardType = {
 }
 
 const SolutionList = () => {
+    const [currentSlide, setCurrentSlide] = React.useState(0)
+    const [loaded, setLoaded] = useState(false)
     const {data, error, isLoading} = useQuery({
         queryKey: ["solutions"],
         queryFn: () => getSolutions(),
     })
 
-    const [ref] = useKeenSlider<HTMLDivElement>({
+    const [ref, instanceRef] = useKeenSlider<HTMLDivElement>({
+        initial: 0,
+        slideChanged(slider) {
+            setCurrentSlide(slider.track.details.rel)
+        },
+        created() {
+            setLoaded(true)
+        },
         breakpoints: {
             "(min-width: 400px)": {
                 slides: {perView: 1, spacing: 16},
@@ -37,9 +48,6 @@ const SolutionList = () => {
                 slides: {perView: 2, spacing: 32},
             },
             "(min-width: 1080px)": {
-                slides: {perView: 3, spacing: 32},
-            },
-            "(min-width: 1536px)": {
                 slides: {perView: 3, spacing: 32},
             },
         },
@@ -69,6 +77,41 @@ const SolutionList = () => {
                     />
                 </div>
             ))}
+            {loaded && instanceRef.current && (
+                <>
+                    <button
+                        onClick={(e: any) =>
+                            e.stopPropagation() || instanceRef.current?.prev()
+                        }
+                        disabled={currentSlide === 0}
+                        className={clsx(
+                            "absolute top-1/2 left-1 -translate-y-1/2 z-20 text-featured-muted disabled:opacity-0 disabled:pointer-events-none"
+                        )}
+                        aria-label="Previous slide"
+                    >
+                        <ArrowLeft size={40}/>
+                    </button>
+                    <button
+                        onClick={(e: any) =>
+                            e.stopPropagation() || instanceRef.current?.next()
+                        }
+                        disabled={
+                            currentSlide ===
+                            instanceRef.current.track.details.slides.length - 1
+                        }
+                        className={clsx(
+                            "absolute top-1/2 right-1 -translate-y-1/2 z-20 text-featured-muted", "disabled:opacity-0 disabled:pointer-events-none"
+                        )}
+                        aria-label="Next slide"
+                    >
+                        <ArrowRight size={40}/>
+                    </button>
+
+                    <ArrowRight
+
+                    />
+                </>
+            )}
         </div>
     );
 };
